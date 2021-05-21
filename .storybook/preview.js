e89127c2a2b779ac22775c9760618badedf4bf0d
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-05-21 17:17:48 +0900

    remove vanilla css use emotion global to inject global css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,24 @@
-import '../src/index.css';
 import { Provider } from 'react-redux';
 import store from '../src/store';
+
+import { injectGlobal } from '@emotion/css';
+
+injectGlobal`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  font-family: league-gothic, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}`;
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { injectGlobal } from '@emotion/css';
 
-import 'index.css';
 import { App } from 'App';
 import store from 'store';
+
+injectGlobal`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  font-family: league-gothic, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}`;
 
 ReactDOM.render(
   <Provider store={store}>
