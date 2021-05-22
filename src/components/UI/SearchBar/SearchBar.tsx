ece4820f9d6ea4f8e29c73a683dadb470b910689
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-05-22 12:59:24 +0900

    search can be done from input field

--- src/components/UI/SearchBar/SearchBar.tsx
+++ src/components/UI/SearchBar/SearchBar.tsx
@@ -1,10 +1,22 @@
 import { Form, Label, Input } from 'components/UI/SearchBar/SearchBar.styles';
+import { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const SearchBar: React.FC<{}> = props => {
+  const history = useHistory();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    history.replace(`/?city=${inputRef.current!.value}`);
+    inputRef.current!.value = '';
+  };
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
       <Label>Search Different Location</Label>
-      <Input type="text" placeholder="City Name"></Input>
+
+      <Input ref={inputRef} type="text" placeholder="City Name"></Input>
     </Form>
   );
 };
